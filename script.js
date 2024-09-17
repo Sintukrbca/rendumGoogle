f0674ef7ff125b68fe5bbab3812ba1ae312f4a33
Sintu Kumar <159878371+Sintukrbca@users.noreply.github.com>
2024-09-17 19:43:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
-const searc = document.getElementById("search")
-const inpu = document.getElementsByClassName("input")
 
-searc.addEventListener('click', () => {
-    window.location.href = `https://www.google.co.in/search?q=${inpu[0].value}`;
+        const searc = document.getElementById("search")
+        const inpu = document.getElementsByClassName("input")
+        searc.addEventListener('click', () => {
+            window.location.href = `https://www.google.co.in/search?q=${inpu[0].value}`;
 
-});
-
-searc.addEventListener('clickEnter', () => {
-    window.location.href = `https://www.google.co.in/search?q=${inpu[0].value}`;
-});
+        });
+        
